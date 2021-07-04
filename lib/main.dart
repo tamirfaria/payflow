93a3b890e01d2f50ecabb4f8185373ff52fd33ff
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-04 13:59:33 -0300

    refactor: add LoginPage in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppWidget());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'PayFlow',
-      home: Scaffold(),
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const LoginPage(),
     );
   }
 }
